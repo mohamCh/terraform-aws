368f2099d1cb76661b1e8fe9935b1308f493b6a6
mohamCh <106377845+mohamCh@users.noreply.github.com>
2024-03-08 00:29:58 +0100

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -10,3 +10,25 @@ resource "aws_subnet" "public_subnet" {
     Name = "public_subnet"
   }
 }
+
+resource "aws_subnet" "public_subnet_client1" {
+  vpc_id                  = aws_vpc.vpc_client1.id
+  cidr_block              = "10.1.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "public_subnet_clinet1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_client2" {
+  vpc_id                  = aws_vpc.vpc_client2.id
+  cidr_block              = "10.2.0.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "public_subnet_client2"
+  }
+}

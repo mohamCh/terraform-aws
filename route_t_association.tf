e23952c371d314897ddb69e3e504ac5eb8b72efb
mohamCh <106377845+mohamCh@users.noreply.github.com>
2024-03-08 00:30:17 +0100

    Update route_t_association.tf

--- route_t_association.tf
+++ route_t_association.tf
@@ -4,3 +4,13 @@ resource "aws_route_table_association" "rta_main" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.rt_main.id
 }
+
+resource "aws_route_table_association" "rta_client1" {
+  subnet_id      = aws_subnet.public_subnet_client1.id
+  route_table_id = aws_route_table.rt_client1.id
+}
+
+resource "aws_route_table_association" "rta_client2" {
+  subnet_id      = aws_subnet.public_subnet_client2.id
+  route_table_id = aws_route_table.rt_client2.id
+}

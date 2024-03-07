a8f88caf139be3443f706a545708dd92343cdddb
mohamCh <106377845+mohamCh@users.noreply.github.com>
2024-03-08 00:31:02 +0100

    Update securityGroup.tf

--- securityGroup.tf
+++ securityGroup.tf
@@ -1,4 +1,4 @@
-#------------------ SG ----------------------
+#------------------ SG admin ----------------------
 resource "aws_security_group" "sg_main" {
   vpc_id = aws_vpc.vpc_main.id
 
@@ -33,5 +33,82 @@ resource "aws_security_group" "sg_main" {
   tags = {
     name        = "sg_main"
     description = "sg_main"
+  }
+}
+
+#------------------ SG client1 ----------------------
+resource "aws_security_group" "sg_client1" {
+  vpc_id = aws_vpc.vpc_client1.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2379
+    to_port     = 2379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name        = "sg_client1"
+    description = "sg_client1"
+  }
+}
+
+#------------------ SG client2 ----------------------
+
+resource "aws_security_group" "sg_client2" {
+  vpc_id = aws_vpc.vpc_client2.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2379
+    to_port     = 2379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name        = "sg_client2"
+    description = "sg_client2"
   }
 }

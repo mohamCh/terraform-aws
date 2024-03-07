51d1cfec0a78452caf56939da799cbe87040417e
mohamCh <106377845+mohamCh@users.noreply.github.com>
2024-03-08 00:30:30 +0100

    Update ig.tf

--- ig.tf
+++ ig.tf
@@ -6,3 +6,19 @@ resource "aws_internet_gateway" "gw_main" {
     Name = "gw_main"
   }
 }
+
+resource "aws_internet_gateway" "gw_client1" {
+  vpc_id = aws_vpc.vpc_client1.id
+
+  tags = {
+    Name = "gw_client1"
+  }
+}
+
+resource "aws_internet_gateway" "gw_client2" {
+  vpc_id = aws_vpc.vpc_client2.id
+
+  tags = {
+    Name = "gw_client2"
+  }
+}

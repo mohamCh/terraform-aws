b11751adcd3847bea31ae8c3e8d805aab1077e55
mohamCh <106377845+mohamCh@users.noreply.github.com>
2024-03-08 00:31:23 +0100

    Update route_t.tf

--- route_t.tf
+++ route_t.tf
@@ -12,3 +12,26 @@ resource "aws_route_table" "rt_main" {
     Name = "rt_main"
   }
 }
+
+resource "aws_route_table" "rt_client1" {
+  vpc_id = aws_vpc.vpc_client1.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_client1.id
+  }
+  tags = {
+    Name = "rt_client1"
+  }
+}
+
+resource "aws_route_table" "rt_client2" {
+  vpc_id = aws_vpc.vpc_client2.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_client2.id
+  }
+  tags = {
+    Name = "rt_client2"
+  }
+}
